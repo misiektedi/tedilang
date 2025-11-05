af3b8a9b975b61c929f1ec1594cf9d4d5c2539e5
Michał Olkuski <michal@olkuski.com>
2025-11-05 02:01:55 +0100

    Calculation support fix

--- src/core/calc.cpp
+++ src/core/calc.cpp
@@ -6,38 +6,6 @@
 std::string chars = "+-*/";
 std::string numbers = "0123456789";
 
-// int interpret(int final, std::string instruction, int offset) {
-//     size_t instruction_length = instruction.length();
-
-//     //std::string instruction_striped = instruction.substr(offset);
-
-//     size_t operation_pos = instruction.find_first_of(chars, offset);
-//     if (operation_pos == std::string::npos) {
-//         return final;
-//     }
-
-//     size_t next_number_pos = instruction.find_first_of(numbers, operation_pos + 1);
-//     if (next_number_pos == std::string::npos) {
-//         return final;
-//     }
-
-//     final = stoi( instruction.substr(offset, operation_pos - offset) );
-
-//     int next_number = stoi( instruction.substr(next_number_pos, instruction.find_first_of(' ', offset)) );
-
-//     char op = instruction[offset + operation_pos];
-    
-//     if ( op == '+' ) final = final + next_number;
-
-//     if ( offset < instruction_length ) {
-//         final = interpret(final, instruction, next_number_pos);
-//     }
-
-//     std::cout << "---" << std::endl << final << std::endl << instruction << std::endl << next_number_pos << std::endl << instruction_length << std::endl << offset << std::endl << next_number << std::endl << "---" << std::endl;
-
-//     return final;
-// }
-
 int interpret( std::string instruction ) {
     size_t operation_pos = instruction.find_first_of(chars);
     if (operation_pos == std::string::npos) {
