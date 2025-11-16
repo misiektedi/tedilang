6bf5dafdb0f196f0cf35b93dc4013c17f39f9352
Michał Olkuski <michal@olkuski.com>
2025-11-16 15:25:10 +0100

    Changing values of defined variables support

--- src/core/keywords/let.cpp
+++ src/core/keywords/let.cpp
@@ -10,24 +10,26 @@
 #include <iostream>
 
 ReturnType key_let( std::string arg ) {
-    std::array<std::string, 2> types = {"int", "string"};
+    std::array<std::string, 2> types    = {"int", "string"};
 
-    std::string type = arg.substr(0, 0 + arg.find(' ') );
+    std::string type                    = arg.substr(0, 0 + arg.find(' ') );
 
-    std::string content = arg.substr(1 + type.length());
+    std::string content                 = arg.substr(1 + type.length());
 
-    size_t pos = content.find('=');
+    size_t pos                          = content.find('=');
 
-    std::string key = content.substr(0, -1 + pos);
-    std::string value = content.substr(pos + 2);
+    std::string key                     = content.substr(0, -1 + pos);
+    std::string value                   = content.substr(pos + 1);
+
+    value                               = string_trim(value);
 
     if ( is_function( value ) ) {
         FunctionRegistry::instance().run( get_function_name(value), get_function_args(value) );
         value = std::get<std::string>(FunctionRegistry::instance().result);
     }
     
-    if ( type == "int" ) Variables::instance().setInt( key, calc(value) );
-    else if ( type == "string" ) Variables::instance().setString( key, remove_qm(value) );
+    if ( type == "int" )            Variables::instance().setInt( key, calc(value) );
+    else if ( type == "string" )    Variables::instance().setString( key, remove_qm(value) );
 
     return std::monostate{};
 }
--- src/core/tedilang_interpreter.cpp
+++ src/core/tedilang_interpreter.cpp
@@ -8,6 +8,34 @@
 #include "function_registry.hpp"
 #include "keyword_registry.hpp"
 
+#include "variables.hpp"
+#include "core/impl/calc.hpp"
+
+void tedilang_variables_handler( std::string x ) {
+    size_t pos          = x.find('=');
+
+    std::string name    = x.substr(0, pos - 1);
+    std::string value   = x.substr(pos + 1, x.length());
+
+    name                = string_trim(name);
+    value               = string_trim(value);
+
+    if ( is_function( value ) ) {
+        FunctionRegistry::instance().run( get_function_name(value), get_function_args(value) );
+        value = std::get<std::string>(FunctionRegistry::instance().result);
+    }
+
+    switch (Variables::instance().getVarType(name)) {
+        case Variables::Type::INT:
+            Variables::instance().setInt(name, calc(value));
+            break;
+
+        case Variables::Type::STRING:
+            Variables::instance().setString(name, value);
+            break;
+    }
+}
+
 void tedilang_interpreter_dispatch( std::string line ) {
     
     if ( KeywordRegistry::instance().run( get_keyword_name(line), line ) ) return;
@@ -15,6 +43,8 @@ void tedilang_interpreter_dispatch( std::string line ) {
     if ( is_function(line) ) {
         if ( FunctionRegistry::instance().run( get_function_name(line), get_function_args(line) ) ) return;
     }
+
+    if ( Variables::instance().isDeclaredVariable( line.substr( 0, line.find(' ') ) ) ) tedilang_variables_handler(line); return;
 
     tedilang_exception("Interpreter could not recognize a valid instruction.");
 
